5364fc88a884dbb0679fd3ec4aea31fd78442e13
Anto Tom Abraham <antotom53@gmail.com>
2022-09-06 15:18:51 +0530

    initial commit

---
+++ studentFreelance/Controllers/clientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace studentFreelance.Controllers
+{
+    public class clientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
